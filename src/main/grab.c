1709c63828cb3ce891e9fedf846c5be2ff88543b
Paolo Bosetti <paolo.bosetti@unitn.it>
2021-06-09 16:33:58 +0200

    Safer _splitpath_s in grab

--- src/main/grab.c
+++ src/main/grab.c
@@ -62,7 +62,7 @@ int main(int argc, char *const argv[]) {
     #ifdef _WIN32
     {
       char basename[_MAX_FNAME];
-      _splitpath(argv[0], NULL, NULL, basename, NULL);
+      _splitpath_s(argv[0], NULL, 0, NULL, 0, basename, _MAX_FNAME, NULL, 0);
       printf("%s - based on %s\n", basename, mightex_sw_version());
     }
     #else
